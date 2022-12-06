dcfb33adbe91633ee38de4ea86abf1547ab4b165
Anderson Conceição <andersonconceiicao@gmail.com>
2022-12-07 10:13:26 +1100

    feat[infra]: added entity fields mapping and relationship

---
+++ Drivers-Management.Infra/Mappings/DriverMappings.cs
@@ -0,0 +1,37 @@
+using Drivers_Management.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Drivers_Management.Infra.Mappings
+{
+    public class DriverMappings : IEntityTypeConfiguration<Driver>
+    {
+        public void Configure(EntityTypeBuilder<Driver> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Cpf);
+
+            builder.Property(x => x.Cpf)
+                   .HasColumnType("varchar(11)");
+
+
+            builder.Property(x => x.Name)
+                 .HasColumnType("varchar(50)")
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAt)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.UpdateAt)
+                    .ValueGeneratedOnUpdate();
+
+
+
+            builder.HasMany(x => x.Vehicles)
+                    .WithMany(x => x.Drivers)
+                    .UsingEntity<DriverVehicle>();
+
+
+        }
+    }
+}
---
+++ Drivers-Management.Infra/Mappings/VehicleMappings.cs
@@ -0,0 +1,28 @@
+using Drivers_Management.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Drivers_Management.Infra.Mappings
+{
+    public class VehicleMappings : IEntityTypeConfiguration<Vehicle>
+    {
+        public void Configure(EntityTypeBuilder<Vehicle> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasIndex(x => x.Plate);
+
+            builder.Property(x => x.Plate)
+                   .HasColumnType("varchar(6)");
+
+            builder.Property(x => x.Brand)
+               .HasColumnType("varchar(20)")
+              .IsRequired();
+
+            builder.Property(x => x.CreatedAt)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.UpdateAt)
+                    .ValueGeneratedOnUpdate();
+        }
+    }
+}
