c1c140131a4c664e4bccce76a4effea8e79beb87
Anderson Conceição <andersonconceiicao@gmail.com>
2022-12-14 20:45:30 +1100

    feat: started controller vehicle with methods to be implemented

---
+++ Drivers-Management.Application/Controllers/VehicleController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Drivers_Management.Application.Controllers
+{
+    [ApiController]
+    [Route("vehicle")]
+    public class VehicleController : ControllerBase
+    {
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok();
+        }
+        [HttpGet("plate")]
+        public async Task<IActionResult> GetByPlateAsync([FromQuery] string plate)
+        {
+            return Ok();
+        }
+        // TODO : One of the last things to implement 
+        [HttpGet("filter")]
+        public async Task<IActionResult> GetAdvancedFilterAsync()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync()
+        {
+            return NoContent();
+        }
+
+
+
+    }
+}
