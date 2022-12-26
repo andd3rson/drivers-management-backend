2341382008d8cc943c7333f4c8b4f92765927d04
Anderson Conceição <andersonconceiicao@gmail.com>
2022-12-26 18:17:30 +1100

    feat: added fakers about drivers and added getAll tests

--- Drivers-Management.Domain/Services/DriverServices.cs
+++ Drivers-Management.Domain/Services/DriverServices.cs
@@ -12,27 +12,28 @@ namespace Drivers_Management.Domain.Services
         private readonly IDriverRepository _drivers;
         private readonly IVehicleServices _vehicleService;
         private readonly IValidator<Driver> _validator;
-        public DriverServices(IDriverRepository drivers, IValidator<Driver> validator, IVehicleServices vehicleService = null)
+        public DriverServices(IDriverRepository drivers, 
+                IValidator<Driver> validator, IVehicleServices vehicleService)
         {
             _drivers = drivers;
             _validator = validator;
             _vehicleService = vehicleService;
         }
 
-        public async Task<(Driver, bool)> CreateAsync(Driver driver)
-        {
-            var validateModel = await _validator.ValidateAsync(driver);
-            if (!validateModel.IsValid)
-                throw new DomainExceptions("Sorry, something went wrong. Try again later.");
-            driver.CreatedAt = DateTime.UtcNow;
-            var resquest = await _drivers.Create(driver);
-            return (resquest, resquest.Id != 0);
-        }
-
         public async Task<IEnumerable<Driver>> GetAllAsync(int pageNumber, int pageSize)
         {
             pageNumber = pageNumber < 1 ? 1 : pageNumber;
             return await _drivers.GetAllAsync(pageSize, pageNumber);
+        }
+
+        public async Task<(Driver, bool)> CreateAsync(Driver driver)
+        {
+            var validateModel = await _validator.ValidateAsync(driver);
+            if (!validateModel.IsValid)
+                return (driver, false);
+            driver.CreatedAt = DateTime.UtcNow;
+            var resquest = await _drivers.Create(driver);
+            return (resquest, resquest.Id != 0);
         }
 
         public async Task<bool> UpdateAsync(Driver driver)
--- Drivers-Management.Tests/Units/Domain/Services/DriverServicesTest.cs
+++ Drivers-Management.Tests/Units/Domain/Services/DriverServicesTest.cs
@@ -1,28 +1,55 @@
-using Drivers_Management.Domain.Contracts.Services;
+using Drivers_Management.Domain.Contracts.Repository;
 using Drivers_Management.Domain.Models;
+using Drivers_Management.Domain.Services;
+using Drivers_Management.Tests.Fakers;
 using FluentAssertions;
+using FluentValidation;
 using NSubstitute;
+
 namespace Drivers_Management.Tests.Units.Domain.Services
 {
     public class DriverServicesTest
     {
-        private readonly IDriverServices _driverServices;
+
+        private readonly IDriverRepository _driverRepository;
+        private readonly IValidator<Driver> _validator;
         public DriverServicesTest()
         {
-            _driverServices = Substitute.For<IDriverServices>();
+            _driverRepository = Substitute.For<IDriverRepository>();
+            _validator = Substitute.For<IValidator<Driver>>();
         }
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
-        public async Task CreateAsync_ShouldReturnsTrueOrFalse_WhenADriverIsCreate(bool returns)
+
+        [Fact]
+        public async Task GetAllAsync_ShouldReturnsEmpty_WhenHasNoItemOnDataBase()
         {
-            // Given
-            var driver = new Driver();
-            _driverServices.CreateAsync(driver).Returns((driver, returns));
-            // When
-            var sut = await _driverServices.CreateAsync(driver);
-            // Then
-            sut.Item2.Should().Be(returns);
+            //Given
+            var sut = new DriverServices(_driverRepository, _validator, null);
+            //When
+            var response = await sut.GetAllAsync(1, 10);
+            //Then
+            response.Count().Should().Be(0);
+
         }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(7)]
+        public async Task GetAllAsync_ShouldReturnsAList_WhenHasItemOnDataBase(int takes)
+        {
+            //Given
+            _driverRepository.GetAllAsync(takes, Arg.Any<int>())
+                            .Returns(DriversFakers.listDrivers().Take(takes));
+            var sut = new DriverServices(_driverRepository, _validator, null);
+
+            //When
+            var response = await sut.GetAllAsync(1, takes);
+
+            //Then
+            response.Count().Should().Be(takes);
+
+        }
+
     }
 }
