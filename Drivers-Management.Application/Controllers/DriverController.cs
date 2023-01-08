33f4226aa9942b6ac6fec18c8ea0da8a303661d5
Anderson Conceição <andersonconceiicao@gmail.com>
2023-01-09 09:53:26 +1100

    feat: added elastic and kibana into DockerCompose || set default keys on application settings

--- Drivers-Management.Application/Controllers/DriverController.cs
+++ Drivers-Management.Application/Controllers/DriverController.cs
@@ -24,15 +24,13 @@ namespace Drivers_Management.Application.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAsync([FromQuery] int pageSize = 50, [FromQuery] int pageNumber = 1)
             => Ok(_mapper.Map<IEnumerable<DriverResponse>>(await _driverServices.GetAllAsync(pageNumber, pageSize)));
-        
-        
-        // RETURN A FILTER LIST.
+
+
+
         [HttpGet("{cpf}")]
         public async Task<IActionResult> GetByCpfAsync(string cpf)
         {
             var result = await _driverServices.GetByCpfAsync(cpf);
-            if (result is null)
-                return NotFound();
             return Ok(_mapper.Map<DriverResponse>(result));
         }
 
@@ -54,7 +52,6 @@ namespace Drivers_Management.Application.Controllers
             => await _driverServices.Vinculate(idDriver, idVehicle) ? Ok() : BadRequest();
 
 
-        // Apply tests
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateAsync(int id, [FromBody] Driver driver)
         {
